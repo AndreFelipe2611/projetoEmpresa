921cb73ec36430a4a1af2cdb456829ac5cdbe0cb
André Felipe <andre1104mg@gmail.com>
2025-04-19 22:14:18 -0300

    tudo responsivo

--- index.php
+++ index.php
@@ -42,6 +42,7 @@ foreach ($itens as $item) {
     <meta charset="UTF-8">
     <title>ANALISTA CSC</title>
     <link rel="stylesheet" href="./style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <div class="container">
